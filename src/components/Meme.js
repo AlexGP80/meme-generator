1a792040bb73a14220b865f4eb46ba2cc43bd3a5
AlexGP80 <alejandrogamino@protonmail.com>
2022-06-22 10:49:05 +0200

    cleaning and clarity

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,9 +5,11 @@ import memesData from "../memesData.js"
 
 export default function Meme() {
 
-    function handleClick() {
-        const index = Math.floor(Math.random() * 101);
-        console.log(memesData.data.memes[index].url);
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const index = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[index].url;
+        console.log(url);
     }
 
     return (
@@ -17,7 +19,7 @@ export default function Meme() {
                     <input type="text" placeholder="Top text" className="upper-text-input"/>
                     <input type="text" placeholder="Bottom text" className="bottom-text-input"/>
                 </div>
-                <button onClick={handleClick}>Get a new meme image 🖼️</button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼️</button>
             </div>
             <img src="../images/memeimg.png" alt="Meme" className="meme-image" />
         </div>
