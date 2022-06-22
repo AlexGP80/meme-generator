0b17803031015712da731eabf9a3574ed00aa577
AlexGP80 <alejandrogamino@protonmail.com>
2022-06-22 10:42:42 +0200

    Challenge: Get a random image from the `memesData` array      when the "new meme image" button is clicked.
         Log the URL of the image to the console. (Don't worry
         about displaying the image yet)

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "../styles/Meme.css";
+import memesData from "../memesData.js"
+
 
 export default function Meme() {
+
+    function handleClick() {
+        const index = Math.floor(Math.random() * 101);
+        console.log(memesData.data.memes[index].url);
+    }
+
     return (
         <div className="meme-form-container">
-            <form className="meme-form">
+            <div className="meme-form">
                 <div className="meme-form-inputs">
-                    <input type="text" placeholder="Upper text" className="upper-text-input"/>
+                    <input type="text" placeholder="Top text" className="upper-text-input"/>
                     <input type="text" placeholder="Bottom text" className="bottom-text-input"/>
                 </div>
-                <button>Get a new meme image 🖼️</button>
-            </form>
+                <button onClick={handleClick}>Get a new meme image 🖼️</button>
+            </div>
             <img src="../images/memeimg.png" alt="Meme" className="meme-image" />
         </div>
     );
